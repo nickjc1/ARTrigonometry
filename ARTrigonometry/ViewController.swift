ddd658f329108d3e29ccba2327a7f24d72e1acd9
CHAO JIANG <cjiang@CHAOs-MacBook-Pro.local>
2019-02-01 15:54:38 -0600

    start project

---
+++ ARTrigonometry/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  ARTrigonometry
+//
+//  Created by CHAO JIANG on 2/1/19.
+//  Copyright © 2019 CHAO JIANG. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sceneView.session.run(configuration)
+        
+        sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        
+    }
+
+
+}
+
