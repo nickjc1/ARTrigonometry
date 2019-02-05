1cef02fc9c0f83f2e5594661365f3c75b56c7f77
CHAO JIANG <cjiang@CHAOs-MacBook-Pro.local>
2019-02-05 15:38:21 -0600

    light added, added 2 movebuttons, added animation feature by adding myrootnode in the middle of the circle, also add a buttons array to install all buttons for each section so that they can be hidden as we need.

--- ARTrigonometry/ViewController.swift
+++ ARTrigonometry/ViewController.swift
@@ -15,8 +15,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var sectionButton: UIButton!
     @IBOutlet weak var sceneView: ARSCNView!
     @IBOutlet weak var picker: UIPickerView!
+    @IBOutlet weak var clockWiseButton: UIButton! //for section1
+    @IBOutlet weak var countClockWiseButton: UIButton!
     
-    let pickerContent: [String] = ["section1"]
+    let pickerContent: [String] = ["section1", "section2", "section3"]
+    var buttons = [UIButton]()
+    
+    
+    var theNodes: [SCNNode]? // store some important scnNodes generate from function in extension for later use.
     
     let configuration = ARWorldTrackingConfiguration()
     
@@ -26,18 +32,49 @@ class ViewController: UIViewController {
         sceneView.session.run(configuration)
         
         sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        sceneView.autoenablesDefaultLighting = true
         
         //set up pickview delegate and datasource
         picker.delegate = self
         picker.dataSource = self
         picker.isHidden = true
         sectionButton.isHidden = false
+//        clockWiseButton.isHidden = true
+//        countClockWiseButton.isHidden = true
+        
+        //add each section's buttons into an array for hidden when touch section selector button
+        buttons = [clockWiseButton, countClockWiseButton]
+        for button in buttons {
+            button.isHidden = true
+        }
+        
     }
     
     @IBAction func sectionButtonClicked(_ sender: UIButton) {
         picker.isHidden = false
         sectionButton.isHidden = true
+        
+        for button in buttons {
+            button.isHidden = true
+        }
+        
+        theNodes = []
+        
     }
+    
+    //movebutton for section1
+    @IBAction func clockWiseClicked(_ sender: UIButton) {
+        if let myRootNode = theNodes?[0] {
+             action(node: myRootNode, countWise: true)
+        }
+    }
+    
+    @IBAction func countClockWiseClicked(_ sender: UIButton) {
+        if let myRootNode = theNodes?[0] {
+            action(node: myRootNode, countWise: false)
+        }
+    }
+    
     
 }
 
@@ -64,27 +101,41 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     }
 }
 
-
+//: MARK ARObject for section1
 extension ViewController {
     func arAnimation(for _row: Int) {
         if _row == 0 {
-            unitCircle()
+            theNodes = unitCircle()
+            clockWiseButton.isHidden = false
+            countClockWiseButton.isHidden = false
+        } else if _row == 1 {
+            theNodes = []
         }
     }
     
-    func unitCircle() {
+    func unitCircle() -> [SCNNode] {
+        
+        var nodes = [SCNNode]()
+        
         let circle = SCNNode()
         circle.geometry = SCNTorus(ringRadius: 0.1, pipeRadius: 0.003)
         circle.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        circle.geometry?.firstMaterial?.specular.contents = UIColor.white
         circle.eulerAngles = SCNVector3(90.degreesToRadians, 0, 0)
         circle.position = SCNVector3(0, 0, -0.5)
         self.sceneView.scene.rootNode.addChildNode(circle)
         
+        let myRootNode = SCNNode()
+        myRootNode.position = SCNVector3(0, 0, -0.5)
+        self.sceneView.scene.rootNode.addChildNode(myRootNode)
+        nodes.append(myRootNode)
+        
         let point = SCNNode()
         point.geometry = SCNSphere(radius: 0.005)
         point.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+        point.geometry?.firstMaterial?.specular.contents = UIColor.white
         point.position = SCNVector3(0.1, 0, 0)
-        circle.addChildNode(point)
+        myRootNode.addChildNode(point)
         
         let hCoordinate = SCNNode()
         hCoordinate.geometry = SCNCylinder(radius: 0.002, height: 0.3)
@@ -113,6 +164,30 @@ extension ViewController {
         vArrow.eulerAngles = SCNVector3(-90.degreesToRadians, 0, 0)
         vArrow.position = SCNVector3(0, 0, -0.155)
         circle.addChildNode(vArrow)
+        
+        let line = SCNNode()
+        line.geometry = SCNCylinder(radius: 0.003, height: 0.1)
+        line.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+        line.geometry?.firstMaterial?.specular.contents = UIColor.white
+        line.eulerAngles = SCNVector3(0, 0, 90.degreesToRadians)
+        line.position = SCNVector3(0.05, 0, 0)
+        myRootNode.addChildNode(line)
+        
+        return nodes
+    }
+    
+    func action(node: SCNNode, countWise: Bool) {
+        var rotateDegree: CGFloat
+        
+        if countWise {
+            rotateDegree = CGFloat(15.degreesToRadians)
+        } else {
+            rotateDegree = CGFloat(-15.degreesToRadians)
+        }
+        
+        let action = SCNAction.rotateBy(x: 0, y: 0, z: rotateDegree, duration: 1)
+
+        node.runAction(action)
     }
     
 }
